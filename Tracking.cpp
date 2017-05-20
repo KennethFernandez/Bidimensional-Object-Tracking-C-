7f92cc249bb497687d25ca94acb93a0a6c36728a
kennethfernandez <kenneth1207@gmail.com>
2017-05-20 16:42:00 -0600

    Change the functions of the class Tracking

--- Tracking.cpp
+++ Tracking.cpp
@@ -2,53 +2,91 @@
 
 Tracking::Tracking(void) {}
 
-void Tracking::KeyDescripExtract(cv::Mat img, std::vector<cv::KeyPoint> &keypoints, cv::Mat &descriptors)
+void Tracking::KeyDescripExtract(const cv::Mat img, std::vector<cv::KeyPoint> &keypoints, cv::Mat &descriptors)
 {
 	//-- Step 1: Detect the keypoints using SIFT Detector
-	cv::Ptr<cv::FeatureDetector> detector = FeatureDetector::create("SIFT");
+	cv::Ptr<cv::FeatureDetector> detector = cv::FeatureDetector::create("SIFT");
+	detector->detect(img, keypoints);
 
+	//-- Step 2: Calculate descriptors (feature vectors)
+	cv::Ptr<cv::DescriptorExtractor> extractor = cv::DescriptorExtractor::create("SIFT");
 
-    detector->detect(img, keypoints);
-
-    //-- Step 2: Calculate descriptors (feature vectors)
-    cv::Ptr<cv::DescriptorExtractor> extractor = DescriptorExtractor::create("SIFT");
-
-
-    extractor->compute(img, keypoints, descriptors);
+	extractor->compute(img, keypoints, descriptors);
 
 }
 
 
-void Tracking::DescripMatcher(cv::Mat descriptors_object, cv::Mat descriptors_scene, std::vector< cv::DMatch > &matches)
+void Tracking::DescripMatcher(const cv::Mat descriptorsImg,const cv::Mat descriptorsMask, std::vector<cv::DMatch> &matches)
 {
 	//-- Step 3: Matching descriptor vectors using FLANN matcher
-    cv::Ptr<cv::DescriptorMatcher> matcher = DescriptorMatcher::create("FlannBased");
+	cv::Ptr<cv::DescriptorMatcher> matcher = cv::DescriptorMatcher::create("FlannBased");
+	
+	//-- Step 4: They will be filtered
+	std::vector<cv::DMatch> firstMatches;
+	matcher->match(descriptorsImg, descriptorsMask, firstMatches);
 
-    std::vector< DMatch > firstMatches; //They will be filtered
+	double max_dist = 0; double min_dist = 100;
 
-    matcher->match(descriptors_object, descriptors_scene, firstMatches);
+	//-- Quick calculation of max and min distances between keypoints
+	for (int i = 0; i < descriptorsImg.rows; i++)
+	{
+		double dist = matches[i].distance;
+		if (dist < min_dist) min_dist = dist;
+		if (dist > max_dist) max_dist = dist;
+	}
 
-    double max_dist = 0; double min_dist = 100;
+	printf("-- Max dist : %f \n", max_dist);
+	printf("-- Min dist : %f \n", min_dist);
 
-    //-- Quick calculation of max and min distances between keypoints
-    for (int i = 0; i < descriptors_object.rows; i++)
-    {
-        double dist = matches[i].distance;
-        if (dist < min_dist) min_dist = dist;
-        if (dist > max_dist) max_dist = dist;
-    }
-
-    printf("-- Max dist : %f \n", max_dist);
-    printf("-- Min dist : %f \n", min_dist);
-
-    //-- Draw only "good" matches (i.e. whose distance is less than 3*min_dist )
-    std::vector< cv::DMatch > matches;
-
-    for (int i = 0; i < descriptors_object.rows; i++)
-    {
-        if (firstMatches[i].distance < 3 * min_dist)
-        {
-            matches.push_back(firstMatches[i]);
-        }
-    }
+	for (int i = 0; i < descriptorsImg.rows; i++)
+	{
+		if (firstMatches[i].distance < 3 * min_dist)
+		{
+		    matches.push_back(firstMatches[i]);
+		}
+	}
 }
+
+void Tracking::DrawMatches(const std::vector<cv::DMatch> matches,const std::vector<cv::KeyPoint> keypointsImg,
+			   const std::vector<cv::KeyPoint> keypointsMask, const cv::Mat imgMat, const cv::Mat maskMat)
+{
+
+	cv::Mat imgMatches;
+	cv::drawMatches(imgMat, keypointsImg, maskMat, keypointsMask,
+			matches, imgMatches, cv::Scalar::all(-1), cv::Scalar::all(-1),
+			std::vector<char>(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+
+	//-- Localize the object
+	std::vector<cv::Point2f> img;
+	std::vector<cv::Point2f> mask;
+
+	for (std::size_t i = 0; i < matches.size(); i++)
+	{
+		//-- Get the keypoints from the good matches
+		img.push_back(keypointsImg[matches[i].queryIdx].pt);
+		mask.push_back(keypointsMask[matches[i].trainIdx].pt);
+	}
+
+	cv::Mat H = cv::findHomography(img, mask, CV_RANSAC);
+
+	//-- Get the corners from the image_1 ( the object to be "detected" )
+	std::vector<cv::Point2f> imgCorners(4);
+	imgCorners[0] = cvPoint(0, 0); 
+	imgCorners[1] = cvPoint(imgMat.cols, 0);
+	imgCorners[2] = cvPoint(imgMat.cols, imgMat.rows); 
+	imgCorners[3] = cvPoint(0, imgMat.rows);
+	std::vector<cv::Point2f> maskCorners(4);
+
+	cv::perspectiveTransform(imgCorners, maskCorners, H);
+
+	//-- Draw lines between the corners (the mapped object in the scene - image_2 )
+	line(imgMatches, maskCorners[0] + cv::Point2f(imgMat.cols, 0), maskCorners[1] + cv::Point2f(imgMat.cols, 0), cv::Scalar(0, 255, 0), 4);
+	line(imgMatches, maskCorners[1] + cv::Point2f(imgMat.cols, 0), maskCorners[2] + cv::Point2f(imgMat.cols, 0), cv::Scalar(0, 255, 0), 4);
+	line(imgMatches, maskCorners[2] + cv::Point2f(imgMat.cols, 0), maskCorners[3] + cv::Point2f(imgMat.cols, 0), cv::Scalar(0, 255, 0), 4);
+	line(imgMatches, maskCorners[3] + cv::Point2f(imgMat.cols, 0), maskCorners[0] + cv::Point2f(imgMat.cols, 0), cv::Scalar(0, 255, 0), 4);
+
+	//-- Show detected matches
+	imshow("Good Matches & Object detection", imgMatches);
+
+}
+
--- Tracking.h
+++ Tracking.h
@@ -1,10 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
 #include <iostream>
 
 #include <opencv/cv.h>
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
 class Tracking
 {
@@ -35,13 +38,15 @@ class Tracking
 		 * Inputs: 
 		 * 		Mat img: The matrix of the image from which the key points and descriptors will be extracted
 		 * 
-		 * Outputs:
-		 * 		-------
+		 * InOutputs:
+		 *		std::vector<cv::KeyPoint> &keypoints: The keypoints obtained of the image 
+		 *		cv::Mat &descriptors: The descriptors
+		 *		
 		 * 
 		 * Restrinctions:
 		 * 		-------
 		 *****/
-		void KeyDescripExtract(cv::Mat img, std::vector<cv::KeyPoint> &keypoints, cv::Mat &descriptors);
+		void KeyDescripExtract(const cv::Mat img, std::vector<cv::KeyPoint> &keypoints, cv::Mat &descriptors);
 
 		/******************************************************
 		 * 
@@ -53,13 +58,32 @@ class Tracking
 		 * 		Mat descriptors_object: The descriptors from the object
 		 *		Mat descriptors_scene: The descriptors from the scene
 		 * 
-		 * Outputs:
-		 * 		-------
+		 * InOutputs:
+		 * 		std::vector<cv::DMatch> &matches: Vector with the points obtained
 		 * 
 		 * Restrinctions:
 		 * 		-------
 		 *****/
-		void DescripMatcher(cv::Mat descriptors_object, cv::Mat descriptors_scene, std::vector< cv::DMatch > &matches);
+		void DescripMatcher(const cv::Mat descriptors_object, const cv::Mat descriptors_scene, std::vector<cv::DMatch> &matches);
+	
+		/******************************************************
+		 * 
+		 * Draw the matches to show the result
+		 * 
+		 * **********************************************
+		 * 
+		 * Inputs: 
+		 * 		
+		 * 
+		 * InOutputs:
+		 * 		
+		 * 
+		 * Restrinctions:
+		 * 		-------
+		 *****/
+		void DrawMatches(const std::vector< cv::DMatch > matches,const std::vector<cv::KeyPoint> keypointsImg,
+			   	 const std::vector<cv::KeyPoint> keypointsMask, const cv::Mat imgMat, const cv::Mat maskMat);
+
 	private:
 		
 };
